c3d2c54af2cb641a45b02301f200a0d3015e3d8c
jekahamster <jekahamster@gmail.com>
2020-08-13 03:37:33 +0300

    Added a list of library versions that are used for correct working of the application and modules

---
+++ lib_versions.py
@@ -0,0 +1,17 @@
+import cv2
+import numpy
+import scipy
+import PIL
+import kivy
+import kivymd
+import emnist
+import json
+
+
+print("cv2:   \t", 		cv2.__version__)
+print("numpy: \t", 		numpy.__version__)
+print("scipy: \t", 		scipy.__version__)
+print("PIL:   \t", 		PIL.__version__)
+print("kivy:  \t", 		kivy.__version__)
+print("kivymd:\t", 		kivymd.__version__)
+print("json:  \t", 		json.__version__)
